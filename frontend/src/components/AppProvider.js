f0ce28206bd5fb6c1d05cbe7c83e6d84e7a1edda
Hamid Keshmiri <78935540+hamidkd@users.noreply.github.com>
2021-06-06 20:10:39 -0400

    init AppContext

---
+++ frontend/src/components/AppProvider.js
@@ -0,0 +1,15 @@
+import React, { createContext } from "react";
+
+export const AppContext = createContext();
+
+export const AppProvider = ({ children }) => {
+  const [user, setUser] = useState(null);
+
+  return (
+    <AppContext.Provider value={(user, setUser)}>
+      {children}
+    </AppContext.Provider>
+  );
+};
+
+export default AppProvider;
