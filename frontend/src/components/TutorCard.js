3c94b9e75a29c045dd91b79cf655249ebe045f70
Hamid Keshmiri <78935540+hamidkd@users.noreply.github.com>
2021-06-07 20:00:40 -0400

    fix image in TutorCard

--- frontend/src/components/TutorCard.js
+++ frontend/src/components/TutorCard.js
@@ -3,11 +3,15 @@ import Styled from "styled-components";
 
 const TutorCard = ({ tutor }) => {
   console.log(tutor);
+  const { avatarImg } = tutor;
   return (
     <Div>
       <p>This is a tutor card.</p>
       <p>{tutor.username}</p>
-      <img width="100px" src={tutor.avatarImg} alt="profile-picture" />
+      <img className='avatar'
+        src={"/images/" + tutor.username + ".jpg"}
+        alt="profile-picture"
+      />
     </Div>
   );
 };
@@ -21,5 +25,16 @@ padding: 1rem;
 border-radius: 1rem;
 
 min-height: 200px;
+box-shadow: rgba(100, 100, 111, 0.2) 0px 7px 29px 0px;
+
+&:hover {
+  box-shadow: rgba(100, 100, 111, 0.8) 0px 7px 29px 0px;
+}
+
+.avatar {
+  background: yellow;
+  max-width: 10rem;
+  border-radius: 50%
+}
 
 `;
